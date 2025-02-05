2660d588d717e674233ce37c6ad74a2e6ada888f
swenbao <essence.lost.0b@icloud.com>
2025-02-05 17:39:31 +0800

    practice data validation

--- data_valid.py
+++ data_valid.py
@@ -1,6 +1,20 @@
+# Data validation
 # Request body & Response body
 # most of the time server will return the response body
 # but client don't have to send the request body
 # to send the request body, use POST, PUT, DELETE, PATCH methods
 # send the request body with GET method has undefined behavior, but is supported by FastAPI in extreme use cases
+
+from fastapi import FastAPI
 from pydantic import BaseModel # pydantic is a data validation library
+
+class User(BaseModel):
+    name: str
+    email: str
+    age: int
+
+app = FastAPI()
+
+@app.post("/register/")
+async def register_user(user: User):
+    return user
